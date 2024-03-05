2926591426d3961d544ae218ad32382a17ddda47
AlexJ <o.fomenkov.2021@alumnos.urjc.es>
2024-03-05 17:45:09 +0100

    Añadida la base de datos segunda y lo de la seguridad

--- analisisDatosSSII/creacionDeTablas.py
+++ analisisDatosSSII/creacionDeTablas.py
@@ -24,24 +24,21 @@ import time
 #       fecha datetime
 
 def if_secure(password, hashes):
-    if password in hashes:
-        print("CONTRASEÑA NO SEGURA ENCONTRADA!")
-        return 0
+    for h in hashes:
+        if password==h:
+            print("CONTRASEÑA NO SEGURA ENCONTRADA! la contraseña es: " + password)
+            return 0
     return 1
-
-
 def crear_tabla_users():
 
     # creamos array de hashes
     hashes = []
     palabras = open('palabras_comunes.txt', 'r')
     for palabra in palabras:
-        print(palabra)
-        hashes.append(hash.md5(palabra.encode()).hexdigest())
 
-    print(len(hashes))
-
-
+        hash_pass = hash.md5(palabra[:-1].encode(encoding="utf-8")).hexdigest()
+        hashes.append(hash_pass)
+    print(hashes)
 
     conn_users = sqlite3.connect('users_data_online.db')
     cursor_user = conn_users.cursor()
@@ -89,7 +86,7 @@ def crear_tabla_users():
             telf = "null"
         password = str(datos_usuario['usuarios'][cont][username]["contrasena"])
         complexity = if_secure(password, hashes)
-        print(password)
+
         province = datos_usuario['usuarios'][cont][username]["provincia"]
         if not province:
             province = "null"
@@ -122,9 +119,54 @@ def crear_tabla_users():
             """)
             conn_users.commit()
 
-    test = cursor_user.execute("SELECT hash_password FROM user_data_online;")
-    print(test.fetchall())
+    #test = cursor_user.execute("SELECT pass_complexity FROM user_data_online;")
+    #print(test.fetchall())
     conn_users.close()
 
+def crear_tabla_legal():
+    conn_users = sqlite3.connect('users_data_online.db')
+    cursor_user = conn_users.cursor()
+    cursor_user.execute("DROP TABLE IF EXISTS legal_data_online;")
+    cursor_user.execute("""
+
+                        CREATE TABLE IF NOT EXISTS legal_data_online(
+                        web TEXT PRIMARY KEY,
+                        cookies INTEGER NOT NULL,
+                        aviso INTEGER NOT NULL,
+                        proteccion INTEGER NOT NULL,
+                        creacion INTEGER NOT NULL
+                        );
+                       """)
+    # aquí apertura del json
+    ficheroLegal = open('datos/legal_data_online.json', 'r')
+    legal_datos = json.load(ficheroLegal)
+
+    keys = []
+    for i in range(len(legal_datos['legal'])):
+        for key in legal_datos['legal'][i]:
+            keys.append(key)
+    
+
+    cont = 0
+    for web in keys:
+        web = str(web)
+        cookies = str(legal_datos['legal'][cont][web]['cookies'])
+        aviso = str(legal_datos['legal'][cont][web]['aviso'])
+        proteccion_de_datos = str(legal_datos['legal'][cont][web]['proteccion_de_datos'])
+        creacion = str(legal_datos['legal'][cont][web]['creacion'])
+
+        cont += 1
+
+        cursor_user.execute(f"""
+                                    INSERT INTO legal_data_online(
+                                    web, cookies, aviso, proteccion, creacion)
+                                    VALUES ('{web}', '{cookies}', '{aviso}', '{proteccion_de_datos}', '{creacion}')
+                                    """)
+        conn_users.commit()
+
+
+    conn_users.close()
+
+
 crear_tabla_users()
-#crear_tabla_legal()
+crear_tabla_legal()
