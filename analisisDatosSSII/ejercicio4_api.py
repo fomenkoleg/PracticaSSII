60c70e84f8873edcff4e24a29653e4ada3ec6c67
AlexJ <o.fomenkov.2021@alumnos.urjc.es>
2024-04-24 15:30:30 +0200

    arreglos finales + botones escondidos

--- analisisDatosSSII/ejercicio4_api.py
+++ analisisDatosSSII/ejercicio4_api.py
@@ -9,21 +9,30 @@ def ejercicio4API():
         id_historia = page_json[cont]
         historia = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{id_historia}.json").json()
         if historia['type'] == 'story':
-            if 'url' in historia and len(historia['kids']) >= 3 and 'title' in historia and 'by' in historia:
-                titulo = historia['title']
-                autor = historia['by']
-                url = historia['url']
-                top_comments = historia['kids'][:3]
+            if 'kids' in historia:
+                if 'url' in historia and len(historia['kids']) >= 3 and 'title' in historia and 'by' in historia:
+                    passed = True
+                    top_comments = historia['kids'][:3]
+                    com1 = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{top_comments[0]}.json").json()
+                    com2 = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{top_comments[1]}.json").json()
+                    com3 = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{top_comments[2]}.json").json()
+                    for kid in [com1, com2, com3]:
+                        if 'by' not in kid or 'text' not in kid:
+                            passed = False
+                    if passed:
 
-                #text = historia['text']
-                l.append(f'<h2>{titulo}</h2>Autor: {autor}<br>Leer más: <a href="{url}">{url}</a>')
-                com1 = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{top_comments[0]}.json").json()
-                com2 = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{top_comments[1]}.json").json()
-                com3 = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{top_comments[2]}.json").json()
-                l.append(f'<h4>Top 3 comentarios:</h4>'
-                         f'<br><b>{com1["by"]}</b><br>{com1["text"]}'
-                         f'<br><b>{com2["by"]}</b><br>{com2["text"]}'
-                         f'<br><b>{com3["by"]}</b><br>{com3["text"]}')
+                        titulo = historia['title']
+                        autor = historia['by']
+                        url = historia['url']
+
+
+                        #text = historia['text']
+                        l.append(f'<h2>{titulo}</h2>Autor: {autor}<br>Leer más: <a href="{url}">{url}</a>')
+
+                        l.append(f'<h4>Top 3 comentarios:</h4>'
+                                 f'<br><b>{com1["by"]}</b><br>{com1["text"]}'
+                                 f'<br><b>{com2["by"]}</b><br>{com2["text"]}'
+                                 f'<br><b>{com3["by"]}</b><br>{com3["text"]}')
 
 
         cont += 1
